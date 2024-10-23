26c8b5831f4fafe8d9f0b7c2852ad3dc34283f31
squid1127 <nnfuruyama@gmail.com>
2024-10-23 00:09:51 -0700

    new feature: information channels with yaml config

--- jerry.py
+++ jerry.py
@@ -87,6 +87,7 @@ class Jerry(core.Bot):
         await self.add_cog(JerryGemini(self))
         await self.add_cog(AutoReply(self))
         await self.add_cog(GuildStuff(self))
+        await self.add_cog(InformationChannels(self, "store/info_channels.yaml"))
 
 
 class JerryGemini(commands.Cog):
@@ -692,7 +693,7 @@ class GuildStuff(commands.Cog):
 
     @app_commands.command(
         name="guild",
-        description="Get information about the guild",
+        description="[Experimental] Get information about this guild (server)",
     )
     async def guild_info(self, interaction: discord.Interaction):
         print(f"[GuildStuff] Guild info requested for {interaction.guild.name}")
@@ -779,3 +780,217 @@ class GuildStuff(commands.Cog):
 
     async def cog_status(self) -> str:
         return "Ready"
+
+
+class InformationChannels(commands.Cog):
+    def __init__(self, bot: Jerry, file: str):
+        self.bot = bot
+        self.file = core.TextFile(file)
+
+        self.bot.shell.add_command(
+            "infochannels",
+            cog="InformationChannels",
+            description="Manage information channels",
+        )
+        self.bot.shell.add_command(
+            "ic",
+            cog="InformationChannels",
+            description="Manage information channels (alias for infochannels)",
+        )
+
+    async def check_file(self):
+        print("[InformationChannels] Checking file")
+        if not os.path.exists(self.file.path):
+            print(
+                f"[InformationChannels] File {self.file.path} does not exist, creating..."
+            )
+            self.file.write({})
+
+        contents = self.file.read()
+
+        if contents is None or not contents.get("guilds", None):
+            if contents is None:
+                contents = {}
+
+            print("[InformationChannels] Guilds key missing, creating...")
+            contents["guilds"] = []
+            self.file.write(contents)
+
+            return True
+
+        guilds = contents["guilds"]
+
+        if not isinstance(guilds, list):
+            print("[InformationChannels] Error: Guilds is not a list")
+            await self.bot.shell.log(
+                "Error: Messages is not a list", "InformationChannels", msg_type="error"
+            )
+            return False
+
+        return True
+
+    async def check_then_update(self):
+        print("[InformationChannels] Checking and updating all channels")
+        success = await self.check_file()
+        if not success:
+            raise Exception("Error initializing")
+
+        contents = self.file.read()
+        guilds = contents["guilds"]
+        for guild in guilds:
+            guild['name'] = self.bot.get_guild(guild['id']).name
+            print(
+                f"[InformationChannels] Checking guild {guild.get('name', guild.get('id', 'Unknown'))}"
+            )
+            for channel in guild["channels"]:
+                print(
+                    f"[InformationChannels] Checking channel {channel.get('name', channel.get('id', 'Unknown'))}"
+                )
+                dc_channel = self.bot.get_channel(channel["id"])
+                if dc_channel is None:
+                    print(f"[InformationChannels] Channel {channel} not found")
+                    await self.bot.shell.log(
+                        f"Channel {channel} not found",
+                        "InformationChannels",
+                        msg_type="error",
+                    )
+                    continue
+                
+                # Optimize message entry
+                print(f"[InformationChannels] Optimizing messages for {dc_channel.name}")
+                for message in channel["messages"]:
+                    if message.get("content", None) == None:
+                        message["content"] = ""
+                        
+                
+                channel["name"] = dc_channel.name
+                print(f"[InformationChannels] Found channel {dc_channel.name}, reading messages...")
+                dc_channel_as_dict = await self._channel_to_dict(dc_channel)
+
+                print(f"[InformationChannels] Current messages:\n{dc_channel_as_dict}")
+                print(f"[InformationChannels] Saved messages:\n{channel['messages']}")
+
+                # Check if messages match
+                if dc_channel_as_dict != channel["messages"]:
+                    print("[InformationChannels] Messages do not match, updating...")
+                    await dc_channel.purge(limit=None)
+                    for message in channel["messages"]:
+                        if len(message.get("embeds", [])) > 1:
+                            raise Exception("Too many embeds")
+                        elif len(message.get("embeds", [])) == 1:
+                            embed = self._dict_to_embed(message["embeds"][0])
+                            await dc_channel.send(
+                                content=message.get("content", None), embed=embed
+                            )
+                        else:
+                            await dc_channel.send(content=message.get("content", None))
+                    print("[InformationChannels] Messages updated")
+                    self.bot.shell.log(
+                        f"Messages in channel {dc_channel.mention} updated",
+                        "InformationChannels",
+                        msg_type="success",
+                    )
+                else:
+                    print("[InformationChannels] Messages match")
+
+        self.file.write(contents)
+        return True
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        success = await self.check_file()
+        if success:
+            print("[InformationChannels] Ready")
+        else:
+            print("[InformationChannels] Error initializing")
+
+    async def cog_status(self):
+        success = await self.check_file()
+        if success:
+            return "Ready"
+        else:
+            return "Error initializing"
+
+    async def shell_callback(self, command: core.ShellCommand):
+        if command.name in ["infochannels", "ic"]:
+            sub_command = command.query.split(" ")[0]
+            if sub_command == "update":
+                try:
+                    await self.check_then_update()
+                    await command.log(
+                        "All channels updated",
+                        "InformationChannels",
+                        msg_type="success",
+                    )
+                except Exception as e:
+                    print(f"[InformationChannels] Error updating channels: {e}")
+                    await command.log(
+                        f"Error updating channels: {e}",
+                        "InformationChannels",
+                        msg_type="error",
+                    )
+                return
+
+    async def _channel_to_dict(self, channel: discord.TextChannel):
+        messages = []
+        async for message in channel.history(limit=None):
+            if message.embeds:
+                embeds = []
+                for embed in message.embeds:
+                    embed_dict = {}
+                    if embed.title:
+                        embed_dict["title"] = embed.title
+                    if embed.description:
+                        embed_dict["description"] = embed.description
+                    if embed.color:
+                        embed_dict["color"] = embed.color.value
+                    if embed.footer:
+                        embed_dict["footer"] = embed.footer.text
+                    if embed.author.name:
+                        embed_dict["author"] = {"name": embed.author.name}
+                        if embed.author.icon_url:
+                            embed_dict["author"]["icon_url"] = embed.author.icon_url
+                    if embed.fields:
+                        embed_dict["fields"] = []
+                        for field in embed.fields:
+                            embed_dict["fields"].append(
+                                {
+                                    "name": field.name,
+                                    "value": field.value,
+                                    "inline": field.inline,
+                                }
+                            )
+                    # Keys in alphabetical order
+                    embeds.sort(key=lambda x: x["name"])
+                    embeds.append(embed_dict)
+    
+
+                messages.append({"content": message.content, "embeds": embeds})
+
+        # Invert order as discord returns messages in newest-first order
+        messages.reverse()
+
+        return messages
+
+    def _dict_to_embed(self, data: dict) -> discord.Embed:
+        embed = discord.Embed(
+            title=data.get("title", None),
+            description=data.get("description", None),
+            color=data.get("color", None),
+        )
+        if data.get("footer", None):
+            embed.set_footer(text=data["footer"])
+        if data.get("author", None):
+            if data['author'].get("icon_url", None):
+                embed.set_author(name=data["author"]["name"], icon_url=data["author"]["icon_url"])
+            else:
+                embed.set_author(name=data["author"]["name"])
+        if data.get("fields", None):
+            for field in data["fields"]:
+                embed.add_field(
+                    name=field["name"],
+                    value=field["value"],
+                    inline=field.get("inline", True),
+                )
+
+        return embed
