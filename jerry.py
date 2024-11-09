ab75edbb913f4becd34d65ceecc447c75b2c64ea
squid1127 <120748767+squid1127@users.noreply.github.com>
2024-11-08 16:49:48 -0800

    Add StaticCommands cog with basic API management and help commands

--- jerry.py
+++ jerry.py
@@ -95,6 +95,7 @@ class Jerry(core.Bot):
         await self.add_cog(GuildStuff(self))
         await self.add_cog(InformationChannels(self, "store/info_channels.yaml"))
         await self.add_cog(CubbScratchStudiosStickerPack(self, "communal/css_stickers"))
+        await self.add_cog(StaticCommands(self))
 
 
 class JerryGemini(commands.Cog):
@@ -1924,3 +1925,75 @@ class CubbScratchStudiosStickerPack(commands.Cog):
             await interaction.response.send_message(
                 f"Error loading sticker: {e}", ephemeral=True
             )
+
+
+class StaticCommands(commands.Cog):
+    """Static commands that don't really do much, including api commands"""
+    
+    def __init__(self, bot: Jerry):
+        self.bot = bot
+
+        self.bot.shell.add_command(
+            "api",
+            cog="StaticCommands",
+            description="Manage API keys",
+        )
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("[StaticCommands] Ready")
+        
+    async def cog_status(self):
+        return "Ready"
+    
+    async def shell_callback(self, command: core.ShellCommand):
+        if command.name == "api":
+            await command.log("This command is not yet implemented (Since no commands require API keys)")
+            return
+        
+    @app_commands.command(
+        name="ping-jerry",
+        description="Is Jerry alive?",
+    )
+    async def ping_command(self, interaction: discord.Interaction):
+        # Get latency
+        latency = self.bot.latency * 1000
+        await interaction.response.send_message(f"Pong! 🏓\nLatency: {latency:.2f}ms")
+
+    @app_commands.command(
+        name="help-jerry",
+        description="Get help with Jerry",
+    )
+    async def help_command(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="Jerry Bot",
+            description="I'm Jerry, a bot created by CubbScratchStudios. I'm designed as a server-specific bot, meaning I have features that are unique to each server I'm in. However, I also have some global features that are available in all servers.",
+            color=0xff5c5c,
+
+        )
+
+        embed.add_field(
+            name="Global Commands",
+            value="""Here are some commands that are available in all servers:
+- `/ping-jerry` - Check if Jerry is alive
+- `/help-jerry` - This command
+- `/sticker` - Get a sticker from the CubbScratchStudios sticker pack
+- `/sever` - Get server information such as message count by user. Kinda like a leveling bot but includes all time. (Highly experimental)
+More to come soon!""",
+            inline=False,
+        )
+        embed.add_field(
+            name="Community Server",
+            value="Check out the [CubbScratchStudios Bot Community Server](https://je.fr.to/discord-bot-community) for more information about Jerry and other bots, as well as support and discussion. (We're still setting things up, so please be patient!)",
+            inline=False,
+        )
+        embed.add_field(
+            name="Splat Bot",
+            value="If you want a more general-purpose bot, check out [Splat Bot](https://je.fr.to/splat-bot), a bot that can do a lot of things, including moderation, fun commands, and more!",
+        )
+        embed.set_footer(
+            text="Brought to you by CubbScratchStudios",
+            icon_url="https://je.fr.to/static/css_logo.PNG",
+        )
+        
+        await interaction.response.send_message(embed=embed)
