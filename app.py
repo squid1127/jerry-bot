e0ed27bf70dd5fc1cf47bcf2e4f96f267bb16da8
squid1127 <120748767+squid1127@users.noreply.github.com>
2024-11-29 19:18:16 -0800

    downreport moved to core

--- app.py
+++ app.py
@@ -16,16 +16,19 @@ Environment Variables
 """
 
 from jerry import Jerry # Jerry bot
-from downreport import DownReport # DownReport bot
 
 # environment variables
 from dotenv import load_dotenv
 import os
+import logging
 
-print("[Runner] Running Jerry Bot")
+# Configure logging
+logger = logging.getLogger("runner")
+
+logger.info("Running Jerry Bot")
 
 # Load the environment variables
-print("[Runner] Loading environment variables")
+logger.info("Loading environment variables")
 load_dotenv()
 
 channel = int(os.getenv("JERRY_SHELL"))
@@ -36,26 +39,12 @@ try:
     gemini_channel = int(os.getenv("JERRY_GEMINI_CHANNEL"))
 except TypeError:
     gemini_channel = None
-    print("[Runner] Warning: Gemini channel not set")
+    logger.warning("Gemini channel not set")
 
 jerry = Jerry(discord_token=token, gemini_token=gemini_token, gemini_channel=gemini_channel, shell_channel=channel)
 postgres_pool = os.getenv("POSTGRES_POOL") if os.getenv("POSTGRES_POOL") else 20
 jerry.add_db(os.getenv("POSTGRES_CONNECTION"), os.getenv("POSTGRES_PASSWORD"), int(postgres_pool))
 
 # Start the Jerry bot
-print("[Runner] Running Jerry Bot")
-try:
-    jerry.run()
-except KeyboardInterrupt:
-    print("[Runner] Bot stopped by host")
-    DownReport(token=token, report_channel=channel).report("Jerry Bot stopped by keyboard interrupt, proceeding with shutdown", title="Jerry Bot Stop", msg_type="warn", cog="DownReport")
-    #break
-except ConnectionError as e:
-    print(f"[Runner] Bot stopped by connection error {e}")
-    if e.args[0] == "Database failed after multiple attempts":
-        DownReport(token=token, report_channel=channel).report(f"Jerry Bot has failed to connect to the database after multiple attempts; Attempting a restart.", title="Jerry Bot Database Connection Error", msg_type="error", cog="DownReport")
-    else:
-        DownReport(token=token, report_channel=channel).report(f"Jerry Bot has crashed due to: {e}; Attempting a restart.", title="Jerry Bot Crashed", msg_type="error", cog="DownReport")
-except Exception as e:
-    print(f"[Runner] Bot crashed {e}")
-    DownReport(token=token, report_channel=channel).report(f"Jerry Bot has crashed due to: {e}; Attempting a restart.", title="Jerry Bot Crashed", msg_type="error", cog="DownReport")
+logger.info("Running Jerry Bot")
+jerry.run()
