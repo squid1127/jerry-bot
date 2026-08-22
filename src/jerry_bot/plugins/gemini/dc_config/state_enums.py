b671a1b1830c2201639f961615212fd4c15b24d0
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-08-22 01:14:00 -0700

    feat: implement LLM profile deletion functionality and update UI interactions

--- src/jerry_bot/plugins/gemini/dc_config/state_enums.py
+++ src/jerry_bot/plugins/gemini/dc_config/state_enums.py
@@ -1,17 +1,18 @@
 """State Enums for the Gemini configuration menu"""
 
-from enum import Enum
+from enum import IntEnum
 
 
-class UIState(Enum):
+class UIState(IntEnum):
     """Enum representing the different states of the UI"""
 
     OVERVIEW = 1
     ERROR = 2
 
 
-class LLMProfileTab(Enum):
+class LLMProfileTab(IntEnum):
     """Enum representing the different tabs in the LLM profile modal"""
 
     PROFILE = 1
     FAIL_OVER = 2
+    DELETE = 3
