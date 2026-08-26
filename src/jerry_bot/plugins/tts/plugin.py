4acb8799f3da09ff24fd3083ad9a3a5c90c4b73e
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-08-26 14:19:27 -0700

    chore: streamline and remove unsued imports

--- src/jerry_bot/plugins/tts/plugin.py
+++ src/jerry_bot/plugins/tts/plugin.py
@@ -1,17 +1,14 @@
 """Main Plugin Module for Text-to-Speech."""
 
-from squid_core import Plugin, PluginCog, Framework
-from squid_core.decorators import CLICommandDec, RedisSubscribe
-
-import discord
-from discord import app_commands
-import asyncio
 from pathlib import Path
 
-from .socket import TTSSocketClient
+from squid_core import Framework, Plugin
+
 from .cog import TTSCog
 from .models.manager import ConfigManager
 from .runner import TTSServiceRunner
+from .socket import TTSSocketClient
+
 
 class TTSPlugin(Plugin):
     """Text-to-Speech Plugin for Jerry Bot."""
@@ -19,42 +16,60 @@ class TTSPlugin(Plugin):
     def __init__(self, framework: Framework):
         super().__init__(framework)
         self.path: Path = self.get_working_directory()
-        self.config_manager: ConfigManager = ConfigManager(self.path / "config.yaml", self.logger)
+        self.config_manager: ConfigManager = ConfigManager(
+            self.path / "config.yaml", self.logger
+        )
         self.service_runner: TTSServiceRunner | None = None
-        
-        self.socket_client: TTSSocketClient|None = None
-        self.cog: TTSCog|None = None
-        
+
+        self.socket_client: TTSSocketClient | None = None
+        self.cog: TTSCog | None = None
+
     async def preload(self):
         """Preload the Text-to-Speech Plugin."""
         await self.config_manager.load()
 
-        
     async def load(self):
         """Load the Text-to-Speech Plugin."""
         self.logger.info("Text-to-Speech initializing...")
 
         if self.config is None:
-            raise RuntimeError("Configuration not loaded. Please run the preload method first.")
+            raise RuntimeError(
+                "Configuration not loaded. Please run the preload method first."
+            )
         if not self.config.enabled:
             self.logger.info("Text-to-Speech plugin is disabled in the configuration.")
             return
 
         cwd = self.config.service.cwd if self.config.service.cwd else self.path
-        socket_path = self.config.socket_path if self.config.socket_path.is_absolute() else cwd / self.config.socket_path
-        output_dir = self.config.output_dir if self.config.output_dir.is_absolute() else cwd / self.config.output_dir
-        
-        self.socket_client = TTSSocketClient(socket_path, self.logger, max_concurrent_requests=self.config.max_concurrent_requests)
+        socket_path = (
+            self.config.socket_path
+            if self.config.socket_path.is_absolute()
+            else cwd / self.config.socket_path
+        )
+        output_dir = (
+            self.config.output_dir
+            if self.config.output_dir.is_absolute()
+            else cwd / self.config.output_dir
+        )
+
+        self.socket_client = TTSSocketClient(
+            socket_path,
+            self.logger,
+            max_concurrent_requests=self.config.max_concurrent_requests,
+        )
         self.cog = TTSCog(self, self.socket_client, output_dir, self.config)
         if self.config.service.use:
             self.service_runner = TTSServiceRunner(self.config, cwd)
-            self.logger.info("Starting TTS service... with command: %s", self.service_runner.command)
+            self.logger.info(
+                "Starting TTS service... with command: %s", self.service_runner.command
+            )
             await self.service_runner.start()
             await self.service_runner.wait_for_ready()
             self.logger.info("TTS service started and ready.")
 
         await self.cog.socket_client.connect()
         await self.framework.bot.add_cog(self.cog)
+
 
     async def unload(self):
         """Unload the Text-to-Speech Plugin."""
