f8574ab3d90f4dc9df1a0cb40cc50c74e86815aa
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-08-28 15:18:32 -0700

    feat: add fast directive

--- src/jerry_bot/plugins/tts/models/enums.py
+++ src/jerry_bot/plugins/tts/models/enums.py
@@ -4,13 +4,17 @@ from enum import Enum
 
 
 class PrefixControlDirective(Enum):
-    """Enum for prefix control directives.
-    
+    """
+    Enum for prefix control directives.
+
     Attributes:
         STOP (str): Directive to clear playback queue
         SKIP (str): Directive to skip the current playback
-        RAW (str): Directive to treat process proceeding content as raw text, bypassing normalization"""
+        RAW (str): Directive to treat process proceeding content as raw text, bypassing normalization
+        FAST (str): Directive to increase playback speed of proceeding content to 2x
+    """
 
     STOP = "stop"
     SKIP = "skip"
     RAW = "raw"
+    FAST = "fast"
