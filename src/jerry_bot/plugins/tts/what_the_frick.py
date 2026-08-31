244d09a5a93a4468f219b6a8983b94f647ea653a
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-08-30 22:34:53 -0700

    feat: add garbled nonsense generator script

---
+++ src/jerry_bot/plugins/tts/what_the_frick.py
@@ -0,0 +1,32 @@
+"""Completely unrelated file that makes output that completely breaks the tts system."""
+
+import random
+import argparse
+
+chars = r"""{}|?,.[]();:"'"""
+
+
+def garbled_nonsense(length: int = 100) -> str:
+    """Generate a random string of garbled nonsense characters.
+
+    Args:
+        length (int): The length of the string to generate. Defaults to 100.
+
+    Returns:
+        str: A random string of garbled nonsense characters.
+    """
+    return "".join(random.choice(chars) for _ in range(length))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate garbled nonsense.")
+    parser.add_argument(
+        "-l", "--length", type=int, default=100, help="Length of the string to generate"
+    )
+    args = parser.parse_args()
+
+    print(garbled_nonsense(args.length))
+
+
+if __name__ == "__main__":
+    main()
